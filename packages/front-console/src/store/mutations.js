252526743e26e6bac061b1b49c48fbfb09733f49
聂杞锋 <308561157@qq.com>
2018-02-07 18:27:45 +0800

    update menus

--- packages/front-console/src/store/mutations.js
+++ packages/front-console/src/store/mutations.js
@@ -28,58 +28,54 @@ const mutations = {
     [types.LOGOUT_PRINCIPAL](state, payload) {
         state.userInformation = payload.user;
         state.permissions = payload.permissions;
-        // console.log(payload.menus);
 
 		// 菜单：1、账户管理：account manager 2、流水记录：flow record 3、下单记录：order history
 		// 新增：4、额度管理：amount manager 5、充值记录：recharge record 5、信用账单：credit bill
-
-        const menus = [
-			{
-				action: null,
-				title: "账户管理",
-                menuId: 1,
-                orderSeq: 1,
-                children: [
-					{
-						action: "/main/accountManager/index",
-                        title: "账户信息",
-                        menuId: 102,
-                        orderSeq: 1
-					},{
-                        action: "/main/accountManager/payOrder",
-                        title: "发放流水",
-                        menuId: 103,
-                        orderSeq: 2
-                    },{
-                        action: "/main/accountManager/payOrderReject",
-                        title: "拒绝下单流水",
-                        menuId: 104,
-                        orderSeq: 3
-                    },{
-                        action: "/main/accountManager/amountManager",
-                        title: "额度管理",
-                        menuId: 105,
-                        orderSeq: 4
-                    },
-                    {
-                        action: "/main/accountManager/companyCreditRecord",
-                        title: "充值记录",
-                        menuId: 106,
-                        orderSeq: 5
-                    },
-                    {
-                        action: "/main/accountManager/creditBill",
-                        title: "信用账单",
-                        menuId: 108,
-                        orderSeq: 6
-                    }
-				]
-			}
-        ];
-        state.principalMenu = menus;
-
-
-        // state.principalMenu = payload.menus;
+        // const menus = [
+		// 	{
+		// 		action: null,
+		// 		title: "账户管理",
+         //        menuId: 1,
+         //        orderSeq: 1,
+         //        children: [
+		// 			{
+		// 				action: "/main/accountManager/index",
+         //                title: "账户信息",
+         //                menuId: 102,
+         //                orderSeq: 1
+		// 			},{
+         //                action: "/main/accountManager/payOrder",
+         //                title: "发放流水",
+         //                menuId: 103,
+         //                orderSeq: 2
+         //            },{
+         //                action: "/main/accountManager/payOrderReject",
+         //                title: "拒绝下单流水",
+         //                menuId: 104,
+         //                orderSeq: 3
+         //            },{
+         //                action: "/main/accountManager/amountManager",
+         //                title: "额度管理",
+         //                menuId: 105,
+         //                orderSeq: 4
+         //            },
+         //            {
+         //                action: "/main/accountManager/companyCreditRecord",
+         //                title: "充值记录",
+         //                menuId: 106,
+         //                orderSeq: 5
+         //            },
+         //            {
+         //                action: "/main/accountManager/creditBill",
+         //                title: "信用账单",
+         //                menuId: 108,
+         //                orderSeq: 6
+         //            }
+		// 		]
+		// 	}
+        // ];
+        // state.principalMenu = menus;
+        state.principalMenu = payload.menus;
     },
 	[types.SET_UPLOAD_POLICY](state, payload) {
 		state.uploadPolicy = payload.data
