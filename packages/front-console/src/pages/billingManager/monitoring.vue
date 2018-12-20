81893ee48cc292d45122d827a2b379912b7c3c42
yabin <yabin@wang.local>
2018-12-20 10:34:49 +0800

    落地公司票量监控弹窗完成

--- packages/front-console/src/pages/billingManager/monitoring.vue
+++ packages/front-console/src/pages/billingManager/monitoring.vue
@@ -14,7 +14,7 @@
       </el-form-item>
     </el-form>
     <div>
-      <el-button type="success">录入销售预测票量</el-button>
+      <el-button type="success" @click="upFilBtnClick">录入销售预测票量</el-button>
       <el-button type="info">导出</el-button>
     </div>
     <br>
@@ -100,6 +100,32 @@
       </el-table-column>
     </el-table>
     <p style="color: #aaa">注：“待开票数（由实际流水推算）”仅对先款后票的客户流水进行推算</p>
+    <el-dialog
+      title="本地上传"
+      :visible.sync="upFilePopIsShow"
+      width="600px">
+      <p>
+        请按照模板填写销售预测数据
+        <el-button size="mini" @click="upFileDownMoBanBtnClick">下载模板</el-button>
+        *上传销售预测表格
+      </p>
+      <el-upload
+        class="upload-demo"
+        drag
+        ref="popUpload"
+        accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        action="https://jsonplaceholder.typicode.com/posts/"
+        :auto-upload="false"
+        multiple>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        <div class="el-upload__tip" slot="tip">请上传小于5M的xls或xlsx格式文件</div>
+      </el-upload>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="upFilePopIsShow = false">取 消</el-button>
+        <el-button type="primary" @click="upFilePopOkBtnClick">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -114,12 +140,26 @@
           date: '2016-05-02',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
-        }]
+        }],
+        // 上传文件提示
+        upFilePopIsShow: false
       }
     },
     methods: {
       // 查找按钮点击
       searchBtnClick() {
+
+      },
+      // 上传文件按钮点击
+      upFilBtnClick() {
+        this.upFilePopIsShow = true
+      },
+      // 上传文件确定按钮点击
+      upFilePopOkBtnClick() {
+        this.$refs.popUpload.submit();
+      },
+      // 弹窗下载模板按钮点击
+      upFileDownMoBanBtnClick() {
 
       }
     }
