2bd2effc05d79237117b5f0faee80195f5b6a039
yabin <yabin@wang.local>
2019-01-25 09:50:27 +0800

    Merge branch 'luoDiGongSi_2019.1.17' into release/prod_luoDiGongSi_2019.1.17

--- packages/front-console/src/pages/invoice/list.vue
+++ packages/front-console/src/pages/invoice/list.vue
@@ -86,7 +86,7 @@
             </el-table-column>
             <el-table-column prop="statusName" label="申请状态" width="120"></el-table-column>
             <el-table-column prop="customCompanyName" label="客户名称" width="220"></el-table-column>
-            <el-table-column prop="purpose" label="发票用途" width="220"></el-table-column>
+            <el-table-column prop="remark" label="发票用途" width="220"></el-table-column>
             <el-table-column prop="amount" label="申请金额" width="120"></el-table-column>
             <el-table-column prop="num" label="申请票数" width="120"></el-table-column>
             <el-table-column prop="realAmount" label="实开金额" width="120"></el-table-column>
@@ -549,7 +549,7 @@
                     type: 'warning'
                 }).then(() => {
                     post('/api/invoice-web/invoice/cancel-invoice-order', {
-                        orderId: this.formInvoice.orderNo 
+                        orderId: this.formInvoice.orderNo
                     }).then(() => {
                         // this.$message({
                         //     type: 'success',
