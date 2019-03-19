4249755245cc780006e07089032be5021cd0eb28
[LKP1994] <[1203851623@qq.com]>
2019-03-19 16:00:24 +0800

    添加页面

---
+++ packages/front-console/src/pages/iframe/iframeIndex.vue
@@ -0,0 +1,14 @@
+<template>
+    <iframe class="iframe" src="https://datacenter.aiyuangong.com/embed/query/27/visualization/31?api_key=CumkBQe7STDen2yvC4eUlASRzc8vq8KmCDt88Kyl"></iframe>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+.iframe {
+    width: 100%;
+    height: calc(100vh - 96px);
+}
+</style>
