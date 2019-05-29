b8f1d67d058fdd08e40e68a3e91b3f2d4dd62ff0
nfeng <308561157@qq.com>
2019-05-29 14:07:24 +0800

    合计bug

--- packages/front-console/src/pages/workOrder/invoiceSheet/components/InvoiceForm.vue
+++ packages/front-console/src/pages/workOrder/invoiceSheet/components/InvoiceForm.vue
@@ -244,12 +244,44 @@ export default {
                 "subjectId":''
             })
         },
+        accAdd(arg1, arg2) {
+            var r1, r2, m, c;
+            try {
+                r1 = arg1.toString().split(".")[1].length;
+            }
+            catch (e) {
+                r1 = 0;
+            }
+            try {
+                r2 = arg2.toString().split(".")[1].length;
+            }
+            catch (e) {
+                r2 = 0;
+            }
+            c = Math.abs(r1 - r2);
+            m = Math.pow(10, Math.max(r1, r2));
+            if (c > 0) {
+                var cm = Math.pow(10, c);
+                if (r1 > r2) {
+                    arg1 = Number(arg1.toString().replace(".", ""));
+                    arg2 = Number(arg2.toString().replace(".", "")) * cm;
+                } else {
+                    arg1 = Number(arg1.toString().replace(".", "")) * cm;
+                    arg2 = Number(arg2.toString().replace(".", ""));
+                }
+            } else {
+                arg1 = Number(arg1.toString().replace(".", ""));
+                arg2 = Number(arg2.toString().replace(".", ""));
+            }
+            return (arg1 + arg2) / m;
+        },
         onSubmit(formName) {
             this.$refs[formName].validate((valid) => {
                 if (valid) {
                     let total = 0
                     this.formData.invoiceWorkOrderSubjectParams.forEach(i => {
-                        total += Number(i.amount)
+                        // total += Number(i.amount)
+                        total = this.accAdd(total, i.amount)
                     })
                     this.formData.amount = total
                     const data = JSON.parse(JSON.stringify(this.formData))
