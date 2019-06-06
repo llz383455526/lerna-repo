641ddcc7488572fad97cc9f9caea26f7804e87c3
nfeng <308561157@qq.com>
2019-06-06 17:27:04 +0800

    radio修改

--- packages/front-console/src/pageComponent/settlementRate/index.vue
+++ packages/front-console/src/pageComponent/settlementRate/index.vue
@@ -6,43 +6,23 @@
                 <el-form-item prop="subType" :rules="{ required: true, message: '请选择结算费率类型', trigger: 'change' }">
                     <el-radio-group v-model="serviceCompanyFeeContent.subType">
                         <el-radio label="ratio" :disabled="disabled">固定比例收费</el-radio>
+                        <el-radio label="nonflow">分{{serviceCompanyFeeContent.incomeAmount}}万 - 无流水阶梯报价</el-radio>
+                        <el-radio label="flow">分{{serviceCompanyFeeContent.incomeAmount}}万 - 按流水分阶梯报价</el-radio>
                     </el-radio-group>
-                </el-form-item>
-                <el-form-item v-if="serviceCompanyFeeContent.subType === 'ratio'" :prop="`feeContentMap.no.0.percent`" :rules="{ required: true, message: '请输入结算实发金额', trigger: 'blur' }">
-                    <span style="color: #606266; ">实发金额 * </span>
-                    <el-input v-model="serviceCompanyFeeContent.feeContentMap.no[0].percent" :disabled="disabled" style="width: 150px;">
-                        <template slot="append">% 每笔</template>
-                    </el-input>
-                    <i class="el-icon-question" title="表示按照固定比例来收取服务费。计算公式：实发发金额 * 收费比例 = 服务费"></i>
-                </el-form-item>
+                </el-form-item><br>
+                <ratio v-if="serviceCompanyFeeContent.subType === 'ratio'" :serviceCompanyFeeContent="serviceCompanyFeeContent"></ratio>
+                <!-- <el-form-item prop="subType" :rules="{ required: true, message: '请选择结算费率类型', trigger: 'change' }">
+                    <el-radio-group v-model="serviceCompanyFeeContent.subType">
+                        <el-radio label="nonflow">分{{serviceCompanyFeeContent.incomeAmount}}万 - 无流水阶梯报价</el-radio>
+                    </el-radio-group>
+                </el-form-item> -->
+                <nonflow v-if="serviceCompanyFeeContent.subType === 'nonflow'" :serviceCompanyFeeContent="serviceCompanyFeeContent"></nonflow>
                 <div class="mb25">
-                    <el-form-item prop="subType" :rules="{ required: true, message: '请选择结算费率类型', trigger: 'change' }">
-                        <el-radio-group v-model="serviceCompanyFeeContent.subType">
-                            <el-radio label="nonflow">分{{serviceCompanyFeeContent.incomeAmount}}万 - 无流水阶梯报价</el-radio>
-                        </el-radio-group>
-                    </el-form-item>
-                    <div v-if="serviceCompanyFeeContent.subType === 'nonflow'">
-                        <!-- <div v-for="item in ['whatever']" :key="item">
-                            <el-form-item label="月收入" prop="incomeAmount" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }">
-                                <el-input style="width: 120px;" v-model="serviceCompanyFeeContent.incomeAmount">
-                                    <template slot="append">万</template>
-                                </el-input>
-                                <el-checkbox>含以下</el-checkbox>
-                            </el-form-item>
-                            <nonflow ref="feeContentMap" :ruleForm="{ 
-                                incomeAmount: serviceCompanyFeeContent.incomeAmount,
-                                list: [ serviceCompanyFeeContent.feeContentMap.down[0], serviceCompanyFeeContent.feeContentMap.up[0] ] 
-                            }"></nonflow>
-                        </div> -->
-                        <nonflow :serviceCompanyFeeContent="serviceCompanyFeeContent"></nonflow>
-                    </div>
-                </div>
-                <div class="mb25">
-                    <el-form-item prop="subType" :rules="{ required: true, message: '请选择结算费率类型', trigger: 'change' }">
+                    <!-- <el-form-item prop="subType" :rules="{ required: true, message: '请选择结算费率类型', trigger: 'change' }">
                         <el-radio-group v-model="serviceCompanyFeeContent.subType">
                             <el-radio label="flow">分{{serviceCompanyFeeContent.incomeAmount}}万 - 按流水分阶梯报价</el-radio>
                         </el-radio-group>
-                    </el-form-item>
+                    </el-form-item> -->
                     <div :span="24" v-if="serviceCompanyFeeContent.subType === 'flow'">
                         <div v-for="item in ['down', 'up']" :key="item" class="mb25">
                             <el-form-item label="月收入" :prop="`incomeAmount`" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }">
@@ -63,11 +43,12 @@
 
 <script>
 import radioFee from './ratioFee'
+import ratio from './ratio'
 import nonflow from './nonflow'
 import feeContentMap from './feeContentMap'
 
 export default {
-    components: { radioFee, nonflow, feeContentMap },
+    components: { radioFee, ratio, nonflow, feeContentMap },
     props: {
         serviceCompanyFeeContent: {
             type: Object
