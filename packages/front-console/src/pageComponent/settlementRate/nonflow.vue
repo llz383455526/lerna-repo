6a114967f89fc738b024e70e77fb89e443c3713d
nfeng <308561157@qq.com>
2019-06-06 19:30:55 +0800

    以上

--- packages/front-console/src/pageComponent/settlementRate/nonflow.vue
+++ packages/front-console/src/pageComponent/settlementRate/nonflow.vue
@@ -1,21 +1,19 @@
 <template>
     <div>
-        <el-form-item label="月收入" prop="incomeAmount" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }">
+        <el-form-item label="月收入" prop="incomeAmount" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }" style="width: 250px;">
             <el-input style="width: 120px;" v-model="serviceCompanyFeeContent.incomeAmount">
                 <template slot="append">万</template>
-            </el-input>
-            <el-checkbox>含以下</el-checkbox>
+            </el-input><span class="ml10" style="color: #606266;">(含)以下</span>
         </el-form-item>
         <el-form-item label="实发金额" prop="feeContentMap.down[0].percent" :rules="{ required: true, message: '请输入实发金额', trigger: 'blur' }">
             <el-input style="width: 120px;" v-model="serviceCompanyFeeContent.feeContentMap.down[0].percent">
                 <template slot="append">%</template>
             </el-input>
         </el-form-item><br>
-        <el-form-item label="月收入" prop="incomeAmount" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }">
+        <el-form-item label="月收入" prop="incomeAmount" :rules="{ required: true, message: '请输入月收入', trigger: 'blur' }" style="width: 250px;">
             <el-input style="width: 120px;" v-model="serviceCompanyFeeContent.incomeAmount">
                 <template slot="append">万</template>
-            </el-input>
-            <el-checkbox>含以下</el-checkbox>
+            </el-input><span class="ml10" style="color: #606266;">以上</span>
         </el-form-item>
         <el-form-item label="实发金额" prop="feeContentMap.up[0].percent" :rules="{ required: true, message: '请输入实发金额', trigger: 'blur' }">
             <el-input style="width: 120px;" v-model="serviceCompanyFeeContent.feeContentMap.up[0].percent">
