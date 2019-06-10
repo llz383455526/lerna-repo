136bada3f6b68abb90945348ed616d12ec892c78
nfeng <308561157@qq.com>
2019-06-10 15:25:50 +0800

    销售绑定id

--- packages/front-console/src/pages/agentContract/components/createstep1/salesInfo.vue
+++ packages/front-console/src/pages/agentContract/components/createstep1/salesInfo.vue
@@ -2,8 +2,8 @@
 <template>
     <div>
         <h3 class="green">归属销售联系信息</h3>
-        <el-form-item label="销售姓名" :prop="`${propName}.salesName`">
-            <el-select v-model="contract.datas.salesInfo.salesName" filterable placeholder="请选择" style="width: 400px;" @change="changeSales">
+        <el-form-item label="销售姓名" :prop="`${propName}.salesId`">
+            <el-select v-model="contract.datas.salesInfo.salesId" filterable placeholder="请选择" style="width: 400px;" @change="changeSales">
                 <el-option v-for="item in optionModel.groupUsersList" :key="item.id" :value="item.id" :label="item.name"></el-option>
             </el-select>
         </el-form-item>
@@ -41,6 +41,7 @@ export default {
             })
             this.contract.datas.salesInfo.mobilePhone = obj.mobilephone
             this.contract.datas.salesInfo.email = obj.email
+            this.contract.datas.salesInfo.salesName = obj.name
             // this.contract.datas.salesInfo.mobilePhone = obj.mobilePhone
         }
     },
