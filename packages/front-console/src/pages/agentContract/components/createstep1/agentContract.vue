11fce41762a3fb9f3b9c8d6f421e96e428783eec
nfeng <308561157@qq.com>
2019-06-10 22:05:49 +0800

    合同变更时间限制同步

--- packages/front-console/src/pages/agentContract/components/createstep1/agentContract.vue
+++ packages/front-console/src/pages/agentContract/components/createstep1/agentContract.vue
@@ -63,35 +63,15 @@ export default {
             propName1: 'datas.agentCompanyBaseInfo',
             propName2: 'datas.agentContract',
             pickerOptions:{
+                /**
+                * 范围为： 当前月以及以后月份
+                */
                 disabledDate(time){
-                    const monthMap = {
-                        28: [2],
-                        30: [4,6,9,11],
-                        31: [1,3,5,7,8,10,12]
-                    }
                     let curDate = new Date();
-                    const curMonth = curDate.getMonth()+1;
-                    const preMonth = curMonth - 1 <= 0 ? 12 : curMonth -1; 
-                    const afterMonth = curMonth + 1 >= 13 ? 1 : curMonth + 1;
-                    let curMonthDay,preMonthDay,afterMonthDay;
-                    for(const day in monthMap) {
-                        if (monthMap[day].indexOf(preMonth) > -1) {
-                            preMonthDay = parseInt(day)
-                        }
-                        if (monthMap[day].indexOf(afterMonth) > -1) {
-                            afterMonthDay = parseInt(day)
-                        }
-                        if (monthMap[day].indexOf(curMonth) > -1) {
-                            curMonthDay = parseInt(day)
-                        }
-                    }
-                    
+                    const curMonth = curDate.getMonth();
                     let curDay = curDate.getDate();
-                    let preDaylimit = (preMonthDay + curDay) * 24 * 3600 * 1000;
-                    let afterDaylimit = (afterMonthDay + curMonthDay - curDay) * 24 * 3600 * 1000;
-                    let monthsAgo = curDate.getTime() - preDaylimit;
-                    let monthsLater = curDate.getTime() + afterDaylimit;
-                    return time.getTime() > monthsLater || time.getTime() < monthsAgo;
+                    let beginTime = new Date(curDate.getFullYear(), curMonth, '01').getTime()
+                    return time.getTime() < beginTime;
                 }
             }
         }
